288930497ffb8c8fd773ac6a625c6fc958c9d861
gourab <gourab.saheb100@gmail.com>
2022-06-20 22:49:26 +0530

    Fibonacci

---
+++ Logical Programs/Program.cs
@@ -0,0 +1,29 @@
+﻿
+Console.WriteLine(" Fibonacci Series ");
+
+
+
+int n = Convert.ToInt32(Console.ReadLine());
+int first = 0, second = 1, result, i;
+Console.WriteLine("fibonacci series is:");
+for (i = 0; i < n; i++)
+{
+    if (i <= 1)
+        result = i;
+    else
+    {
+        result = first + second;
+        first = second;
+        second = result;
+    }
+    Console.WriteLine(result);
+
+}
+
+
+
+
+
+
+
+
